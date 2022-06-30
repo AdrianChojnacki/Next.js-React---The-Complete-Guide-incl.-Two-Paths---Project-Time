5d2d3207ac34e3e908bd0777ad9b7a598ab8ba82
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-30 17:21:58 +0200

    changed dummy-data

--- dummy-data.js
+++ dummy-data.js
@@ -1,32 +1,32 @@
 const DUMMY_EVENTS = [
   {
     id: "e1",
-    title: "Programming for everyone",
+    title: "Tibetan trumpets",
     description:
-      "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
+      "Everyone can learn to play them! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with playing as well.",
     location: "Somestreet 25, 12345 San Somewhereo",
     date: "2021-05-12",
-    image: "images/coding-event.jpg",
+    image: "images/trumpets.jpg",
     isFeatured: false,
   },
   {
     id: "e2",
-    title: "Networking for introverts",
+    title: "Dark retreat",
     description:
-      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+      "The dark retreat environment of the Bon religion is particularly conducive to the practice of certain visionary yogas.",
     location: "New Wall Street 5, 98765 New Work",
     date: "2021-05-30",
-    image: "images/introvert-event.jpg",
+    image: "images/dark.jpg",
     isFeatured: true,
   },
   {
     id: "e3",
-    title: "Networking for extroverts",
+    title: "Pilgrimage",
     description:
-      "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
+      "A pilgrimage is a journey, often into an unknown or foreign place, where a person goes in search of new or expanded meaning about their self, others, nature, or a higher good, through the experience.",
     location: "My Street 12, 10115 Broke City",
     date: "2022-04-10",
-    image: "images/extrovert-event.jpg",
+    image: "images/stupa.jpg",
     isFeatured: true,
   },
 ];
